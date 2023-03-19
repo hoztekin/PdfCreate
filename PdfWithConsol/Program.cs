f730cb43aa32c7a8be077ac672985f3fc8c50915
hoztekin <hoztekin81@hotmail.com>
2023-03-19 23:03:19 +0300

    Different packages

---
+++ PdfWithConsol/Program.cs
@@ -0,0 +1,20 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace PdfWithConsol
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			String path = @"D:\C#\PdfCreate\PdfCreate\Document\invoice.pdf";
+			PdfWriter writer = new PdfWriter(path);
+			PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+			document.Add(new Paragraph("Hello World"));
+			document.Close();
+			
+		}
+	}
+}
